1f0017229b84df214c752146cc75e5fc900cda9c
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-11-02 15:56:08 +0100

    Rebuilt the vue app from scratch

--- ClientApp/src/store/index.ts
+++ ClientApp/src/store/index.ts
@@ -1,19 +1,15 @@
 import Vue from 'vue';
-import Vuex, { StoreOptions } from 'vuex';
-import { RootState } from './types';
-import { counter } from './counter/index';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-// Vuex structure based on https://codeburst.io/vuex-and-typescript-3427ba78cfa8
-
-const store: StoreOptions<RootState> = {
+export default new Vuex.Store({
   state: {
-    version: '1.0.0', // a simple property
+  },
+  mutations: {
+  },
+  actions: {
   },
   modules: {
-    counter,
   },
-};
-
-export default new Vuex.Store<RootState>(store);
+});
