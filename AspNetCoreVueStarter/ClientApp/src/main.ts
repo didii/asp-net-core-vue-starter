4273f94990a9eb8cd1de9132219130d892454d3f
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-11-06 23:55:39 +0100

    Removed vuetify again

--- AspNetCoreVueStarter/ClientApp/src/main.ts
+++ AspNetCoreVueStarter/ClientApp/src/main.ts
@@ -3,7 +3,6 @@ import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 import '@/registerServiceWorker';
-import vuetify from '@/plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
@@ -12,6 +11,5 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     store,
-    vuetify,
     render: (h) => h(App),
 }).$mount('#app');
--- AspNetCoreVueStarter/ClientApp/vue.config.js
+++ AspNetCoreVueStarter/ClientApp/vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
     configureWebpack: {
         devtool: 'source-map',
-    },
-    outputDir: 'wwwroot',
-    transpileDependencies: ['vuetify'],
+    }
 };
