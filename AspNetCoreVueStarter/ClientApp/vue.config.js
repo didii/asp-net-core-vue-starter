0505b1a974fc93f67bb09e687a27b3c7fcd47a63
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2019-11-07 08:40:59 +0100

    Move hashed files to webpack-generated folder

--- AspNetCoreVueStarter/ClientApp/vue.config.js
+++ AspNetCoreVueStarter/ClientApp/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     configureWebpack: {
         devtool: 'source-map',
+        output: {
+            devtoolModuleFilenameTemplate: info => {
+                var $filename = 'webpack:///' + info.resourcePath;
+                if (info.resourcePath.match(/\.vue$/) && !info.query.match(/type=script/)) {
+                    $filename = 'webpack-generated:///' + info.resourcePath + '?' + info.hash;
+                }
+                return $filename;
+            },
+            devtoolFallbackModuleFilenameTemplate: 'webpack-generated:///[resource-path]?[hash]', 
+        }
     }
 };
